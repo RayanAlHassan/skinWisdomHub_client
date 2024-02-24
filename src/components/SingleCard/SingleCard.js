9839a068b4c5636cc42eb8d78f3f8db8b7fe40f3
Rayan <rayanalhassan2000@gmail.com>
2024-02-24 21:05:06 +0200

    single card

--- src/components/SingleCard/SingleCard.js
+++ src/components/SingleCard/SingleCard.js
@@ -1,56 +1,90 @@
+// import React from "react";
+// import style from "./SingleCard.module.css";
+// import image from "../../assets/images/serum.jpg";
+
+// const fakeData = [
+//   {
+//     id: 1,
+//     brand: "FAKE BRAND",
+//     img: image,
+//     description:
+//       "A nutrient-rich, lightweight essence that boosts barrier function and provides immediate, luminous hydration. Hailey's essential prep step to calm skin and begin the rhode routine. ",
+//     subCategoryID: "fakeSubCateg1",
+//     skinType: "oil skin",
+//     ingredients: [
+//       {
+//         name: "Fake Ingredient 1",
+//         description: "Description of Fake Ingredient 1",
+//       },
+//       {
+//         name: "Fake Ingredient 2",
+//         description: "Description of Fake Ingredient 2",
+//       },
+//     ],
+//   },
+
+//   // Add more fake data objects as needed
+// ];
+
+// const SingleCard = () => {
+//   return (
+//     <div className={style.container}>
+//       {fakeData.map((item) => (
+//         <div key={item.id} className={style.content}>
+//           <div className={style.left}>
+//             <img className={style.img} src={item.img} alt={item.brand} />
+//           </div>
+//           <div className={style.right}>
+//             <p className={style.subCategory}> {item.subCategoryID}</p>
+//             <p className={style.brand}>BRAND: {item.brand}</p>
+//             <p className={style.skinType}>Skin Type: {item.skinType}</p>
+//             <p className={style.description}>description: {item.description}</p>
+
+//             <ul className={style.ingredients}>
+//               <p className={style.ingredientsTitle}>Ingredients:</p>
+//               {item.ingredients.map((ingredient, index) => (
+//                 <li key={index} className={style.li}>
+//                   {ingredient.name}: {ingredient.description}
+//                 </li>
+//               ))}
+//             </ul>
+//           </div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default SingleCard;
 import React from "react";
 import style from "./SingleCard.module.css";
-import image from "../../assets/images/serum.jpg";
 
-const fakeData = [
-  {
-    id: 1,
-    brand: "FAKE BRAND",
-    img: image,
-    description:
-      "A nutrient-rich, lightweight essence that boosts barrier function and provides immediate, luminous hydration. Hailey's essential prep step to calm skin and begin the rhode routine. ",
-    subCategoryID: "fakeSubCateg1",
-    skinType: "oil skin",
-    ingredients: [
-      {
-        name: "Fake Ingredient 1",
-        description: "Description of Fake Ingredient 1",
-      },
-      {
-        name: "Fake Ingredient 2",
-        description: "Description of Fake Ingredient 2",
-      },
-    ],
-  },
+const SingleCard = ({ product }) => {
+  console.log("Product data:", product); // Log the product object
 
-  // Add more fake data objects as needed
-];
-
-const SingleCard = () => {
   return (
-    <div className={style.container}>
-      {fakeData.map((item) => (
-        <div key={item.id} className={style.content}>
-          <div className={style.left}>
-            <img className={style.img} src={item.img} alt={item.brand} />
-          </div>
-          <div className={style.right}>
-            <p className={style.subCategory}> {item.subCategoryID}</p>
-            <p className={style.brand}>BRAND: {item.brand}</p>
-            <p className={style.skinType}>Skin Type: {item.skinType}</p>
-            <p className={style.description}>description: {item.description}</p>
 
-            <ul className={style.ingredients}>
-              <p className={style.ingredientsTitle}>Ingredients:</p>
-              {item.ingredients.map((ingredient, index) => (
-                <li key={index} className={style.li}>
-                  {ingredient.name}: {ingredient.description}
-                </li>
-              ))}
-            </ul>
-          </div>
+    <div className={style.container}>
+      <div key={product._id} className={style.content}>
+        <div className={style.left}>
+          <img className={style.img} src={product.image} alt={product.brand} />
         </div>
-      ))}
+        <div className={style.right}>
+          <p className={style.subCategory}> {product.subCategoryID}</p>
+          <p className={style.brand}>BRAND: {product.brand}</p>
+          <p className={style.skinType}>Skin Type: {product.skinType}</p>
+          <p className={style.description}>description: {product.description}</p>
+
+          <ul className={style.ingredients}>
+            <p className={style.ingredientsTitle}>Ingredients:</p>
+            {product.ingredients.map((ingredient, index) => (
+              <li key={index} className={style.li}>
+                {ingredient.name}: {ingredient.description}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Reviews/Reviews.js
+++ src/pages/Reviews/Reviews.js
@@ -1,3 +1,4 @@
+
 // import React, { useContext, useEffect, useState } from "react";
 // import axios from "axios";
 // import image from "../../assets/images/cleanse.jpg";
@@ -5,13 +6,21 @@
 // import Rating from "@mui/material/Rating";
 // import { AuthContext } from "../../Context/AuthContext";
 // import StarIcon from "@mui/icons-material/Star";
-
 // import Box from "@mui/material/Box";
+// import Modal from "@mui/material/Modal";
+// import TextField from "@mui/material/TextField";
+// import Button from "@mui/material/Button";
+// import Card from "@mui/material/Card";
+// import CardContent from "@mui/material/CardContent";
 
 // function Reviews() {
 //   const [reviews, setReviews] = useState([]);
 //   const { user } = useContext(AuthContext);
 //   const [comments, setComments] = useState([]);
+//   const [showModal, setShowModal] = useState(false);
+//   const [selectedReviewId, setSelectedReviewId] = useState(null);
+//   const [newComment, setNewComment] = useState("");
+//   const [searchQuery, setSearchQuery] = useState('');
 
 //   useEffect(() => {
 //     async function fetchReviews() {
@@ -26,6 +35,7 @@
 //         console.error("Error fetching reviews:", error);
 //       }
 //     }
+
 //     async function fetchComments() {
 //       try {
 //         const response = await axios.get("http://localhost:5000/comments/");
@@ -85,13 +95,46 @@
 //     handleSubmit(value, id);
 //   };
 
+//   const handleOpenModal = (id) => {
+//     setSelectedReviewId(id);
+//     setShowModal(true);
+//   };
+
+//   const handleCloseModal = () => {
+//     setShowModal(false);
+//   };
+
+//   const handleAddComment = async () => {
+//     try {
+//       const response = await axios.post("http://localhost:5000/comments/create", {
+//         reviewID: selectedReviewId,
+//         feedback: newComment,
+//         userID: user._id,
+//       });
+//       if (response) {
+//         console.log("Comment added:", response.data);
+//         // Update the comments state to include the new comment
+//         setComments([...comments, response.data]);
+//         // Clear the newComment state
+//         setNewComment("");
+//       }
+//     } catch (error) {
+//       console.error("Error adding comment:", error);
+//     }
+//   };
+// //   const filteredProducts = reviews.filter(item => 
+// //     item.name.toLowerCase().includes(searchQuery.toLowerCase()) || 
+// //     (item.subCategoryID && item.subCategoryID.name.toLowerCase().includes(searchQuery.toLowerCase()))
+// // );
 //   return (
 //     <main className={style.container}>
+//       {/* Posts */}
 //       <div className={style.top}>
-//         <img src={image} className={style.img}></img>
+//         <img src={image} className={style.img} alt="background" />
 //       </div>
 //       {reviews.map((rev) => (
 //         <div key={rev._id} className={style.post}>
+//           {/* Post Content */}
 //           <div className={style.postTop}>
 //             <div className={style.topLeft}>
 //               {rev.userID.image ? (
@@ -101,28 +144,19 @@
 //                   alt="profile user"
 //                 />
 //               ) : (
-//                 <img src={image} className={style.profileUser}></img>
+//                 <img src={image} className={style.profileUser} alt="profile user" />
 //               )}
 //               <p className={style.name}>{rev?.userID?.name}</p>
 //             </div>
 //             <div className={style.topCenter}>
-//               <p
-//                 className={style.name}
-//                 style={{ fontWeight: "900", fontSize: "22px" }}
-//               >
+//               <p className={style.name} style={{ fontWeight: "900", fontSize: "22px" }}>
 //                 {rev.productName}
 //               </p>
-//               {/* </div>
-//             <div className={style.topRight}> */}
 //               <p className={style.skinType}>{rev.skinType}</p>
 //             </div>
 //           </div>
 //           <div className={style.center}>
-//             <img
-//               src={`http://localhost:5000/images/${rev.image}`}
-//               className={style.posttt}
-//               alt="rev"
-//             />
+//             <img src={`http://localhost:5000/images/${rev.image}`} className={style.posttt} alt="rev" />
 //           </div>
 //           <div className={style.bottom}>
 //             <p className={style.desc}>{rev.description}</p>
@@ -149,30 +183,52 @@
 //                 </span>
 //                 <StarIcon style={{ color: "yellow" }} /> Out Of 5
 //               </span>
-//               {console.log("rev ID:", reviews[10]._id)}
-//               {console.log("ReviewID:", comments[0].reviewID._id)}
-//               {console.log("ReviewID:", comments[0].feedback)}
-//               {console.log(
-//                 "Filtered Comments:",
-//                 comments.filter((comments) => comments.reviewID._id === rev._id)
-//               )}
-//             {comments.filter((comment) => comment.reviewID._id === rev._id).length > 0 && (
-//       <div className={style.commentsSection}>
-//         {comments
-//           .filter((comment) => comment.reviewID._id === rev._id)
-//           .map((comment) => (
-//             <div key={comment._id} className={style.comment}>
-//               <p>{comment.feedback}</p>
-//             </div>
-//           ))}
-//       </div>
-//     )}
-//               hetuuuuuuuu
-//               {/* //////////////// */}
+//               {/* Comments Button */}
+//               <button className={style.commentButton} onClick={() => handleOpenModal(rev._id)}>View Comments</button>
 //             </div>
 //           </div>
 //         </div>
 //       ))}
+//       {/* Comments Modal */}
+//       <Modal open={showModal} >
+//         <div className={style.modalBackdrop}>
+//           <div className={style.modalContent}>
+//             <h2 className={style.titleCmnt}>Comments</h2>
+//             <div className={style.commentsContainer}>
+//               {/* Comment Cards */}
+//               <div className={style.commentCards}>
+//                 {comments
+//                   .filter((comment) => comment.reviewID._id === selectedReviewId)
+//                   .map((comment) => (
+//                     <Card key={comment._id} className={style.commentCard}>
+//                       <CardContent>
+//                         <p>{comment.feedback}</p>
+//                       </CardContent>
+//                     </Card>
+//                   ))}
+//               </div>
+//               {/* Add Comment Textarea */}
+//               <textarea
+//                 className={style.commentTextArea}
+//                 multiline
+//                 rows={4}
+//                 variant="outlined"
+//                 placeholder="Add a comment..."
+//                 value={newComment}
+//                 onChange={(e) => setNewComment(e.target.value)}
+//               />
+//               {/* Submit Button */}
+//               <button className={style.submitButton} variant="contained" onClick={handleAddComment}>
+//                 Add Comment
+//               </button>
+//             </div>
+//             {/* Close Button */}
+//             <button className={style.closee} onClick={handleCloseModal}>
+//               Close
+//             </button>
+//           </div>
+//         </div>
+//       </Modal>
 //     </main>
 //   );
 // }
@@ -199,35 +255,25 @@ function Reviews() {
   const [showModal, setShowModal] = useState(false);
   const [selectedReviewId, setSelectedReviewId] = useState(null);
   const [newComment, setNewComment] = useState("");
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
-    async function fetchReviews() {
+    async function fetchData() {
       try {
-        const response = await axios.get("http://localhost:5000/reviews/");
-        if (response) {
-          setReviews(
-            response.data.map((review) => ({ ...review, selectedRating: 0 }))
-          );
+        const [reviewsResponse, commentsResponse] = await Promise.all([
+          axios.get("http://localhost:5000/reviews/"),
+          axios.get("http://localhost:5000/comments/")
+        ]);
+        if (reviewsResponse && commentsResponse) {
+          setReviews(reviewsResponse.data.map((review) => ({ ...review, selectedRating: 0 })));
+          setComments(commentsResponse.data);
         }
       } catch (error) {
-        console.error("Error fetching reviews:", error);
+        console.error("Error fetching data:", error);
       }
     }
 
-    async function fetchComments() {
-      try {
-        const response = await axios.get("http://localhost:5000/comments/");
-        if (response) {
-          console.log("data commentssssssssss", response.data);
-          setComments(response.data);
-        }
-      } catch (error) {
-        console.error("Error fetching comments:", error);
-      }
-    }
-
-    fetchReviews();
-    fetchComments();
+    fetchData();
   }, []);
 
   const handleSubmit = async (value, id) => {
@@ -239,7 +285,6 @@ function Reviews() {
       });
       if (response) {
         console.log("Response:", response.data);
-        // Assuming response.data contains updated review data with ratings
         setReviews(
           reviews.map((review) => {
             if (review._id === id) {
@@ -258,7 +303,6 @@ function Reviews() {
   };
 
   const handleChange = (value, id) => {
-    // Update the selected rating for the current post
     setReviews(
       reviews.map((review) => {
         if (review._id === id) {
@@ -291,9 +335,7 @@ function Reviews() {
       });
       if (response) {
         console.log("Comment added:", response.data);
-        // Update the comments state to include the new comment
         setComments([...comments, response.data]);
-        // Clear the newComment state
         setNewComment("");
       }
     } catch (error) {
@@ -301,15 +343,31 @@ function Reviews() {
     }
   };
 
+  const filteredReviews = reviews.filter(review =>
+    review.productName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    review.skinType.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   return (
     <main className={style.container}>
-      {/* Posts */}
+      {/* Background Image */}
       <div className={style.top}>
         <img src={image} className={style.img} alt="background" />
+        <input 
+        type="text" 
+        placeholder="Search For Products Name Or Skin Type" 
+        value={searchQuery} 
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className={style.searchbar} 
+      />
       </div>
-      {reviews.map((rev) => (
+      
+      {/* Search input */}
+     
+  
+      {/* Posts */}
+      {filteredReviews.map((rev) => (
+        
         <div key={rev._id} className={style.post}>
-          {/* Post Content */}
           <div className={style.postTop}>
             <div className={style.topLeft}>
               {rev.userID.image ? (
@@ -345,7 +403,7 @@ function Reviews() {
               >
                 <Rating
                   name={`simple-controlled-${rev._id}`}
-                  value={rev.selectedRating} // Use selectedRating from review object
+                  value={rev.selectedRating}
                   onChange={(e, value) => handleChange(value, rev._id)}
                   max={5}
                   precision={1}
@@ -358,19 +416,18 @@ function Reviews() {
                 </span>
                 <StarIcon style={{ color: "yellow" }} /> Out Of 5
               </span>
-              {/* Comments Button */}
               <button className={style.commentButton} onClick={() => handleOpenModal(rev._id)}>View Comments</button>
             </div>
           </div>
         </div>
       ))}
+  
       {/* Comments Modal */}
-      <Modal open={showModal} >
+      <Modal open={showModal} onClose={handleCloseModal}>
         <div className={style.modalBackdrop}>
           <div className={style.modalContent}>
             <h2 className={style.titleCmnt}>Comments</h2>
             <div className={style.commentsContainer}>
-              {/* Comment Cards */}
               <div className={style.commentCards}>
                 {comments
                   .filter((comment) => comment.reviewID._id === selectedReviewId)
@@ -382,7 +439,6 @@ function Reviews() {
                     </Card>
                   ))}
               </div>
-              {/* Add Comment Textarea */}
               <textarea
                 className={style.commentTextArea}
                 multiline
@@ -392,12 +448,10 @@ function Reviews() {
                 value={newComment}
                 onChange={(e) => setNewComment(e.target.value)}
               />
-              {/* Submit Button */}
-              <button className={style.submitButton} variant="contained" onClick={handleAddComment}>
+              <button className={style.submitButton} onClick={handleAddComment}>
                 Add Comment
               </button>
             </div>
-            {/* Close Button */}
             <button className={style.closee} onClick={handleCloseModal}>
               Close
             </button>
